ce3c0aa2e1355b0acce28218fb5286d52a53a9f0
kismakk <119887923+kismakk@users.noreply.github.com>
2023-10-18 15:41:02 +0300

    Modified Search UI
    upgraded search UI to Bootstrap styling and modified the displayDrinks function to output properly

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -22,19 +22,23 @@ class UI {
             
             let ingredientsOutput = '';
             ingredients.forEach(ingredient => {
-                ingredientsOutput += `<li>${ingredient}</li>`;
+                ingredientsOutput += `<li class="card-text">${ingredient}</li>`;
             });
 
             console.log(ingredients); 
             output += `
-            <div class="card">
-                <div class="card-header">
-                    <h3 class="card-title">${drink.strDrink}</h3>
-                </div>
-                <div class="card-body">
-                    <img src="${drink.strDrinkThumb}" alt="${drink.strDrink}" class="card-img-top">
-                    ${ingredientsOutput}
-                    <p class="card-text">${drink.strInstructions}</p>
+                <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="${drink.strDrinkThumb}" class="img-fluid rounded-start" alt="${drink.strDrink}">
+                        </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">${drink.strDrink}</h5>
+                            <p class="card-text">${ingredientsOutput}</p>
+                            <p class="card-text"><small class="text-body-secondary">${drink.strInstructions}</small></p>
+                        </div>
+                    </div>
                 </div>
             </div>
             `;
@@ -45,6 +49,7 @@ class UI {
 
 async function searchCocktail(e) {
     e.preventDefault();
+    console.log(searchBtn);
     let value = document.getElementById('search-input').value;
     console.log(value);
     let url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${value}`;
