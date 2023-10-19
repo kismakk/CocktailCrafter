ebcb5c5df562fdbbaaf77315402f3cabb0422054
kismakk <119887923+kismakk@users.noreply.github.com>
2023-10-19 14:04:07 +0300

    Created error handling
    Created error handling to display cards if something goes wrong e.g. no cocktail is found. Also some light UI rework

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -92,7 +92,7 @@ class UI {
         let output = '';
         cocktail.forEach(cocktail => {
             output += `
-             <div class="card mb-3">
+             <div class="card mb-3 w-50 mx-auto">
             <div class="row g-0">
                 <div class="col-md-4">
                     <img src="${cocktail.strDrinkThumb}" class="img-fluid rounded-start" alt="Random cocktail">
@@ -113,34 +113,105 @@ class UI {
     }
 }
 
+class Error {
+
+    static errorClass = 'alert alert-danger d-flex align-items-center w-50 mx-auto';
+
+    static displaySearchError() {
+        const div = document.createElement('div');
+        div.className = this.errorClass;
+        div.appendChild(document.createTextNode('Something went wrong, please try again later.'));
+        const container = document.querySelector('#search-container');
+        if (URL.includes("search.html")) {
+            const search = document.querySelector('#search-results');
+            container.insertBefore(div, search);
+            setTimeout(() => document.querySelector('.alert').remove(), 3000);
+        }
+        else {
+            document.getElementById('cocktail-card-home').innerHTML = output;
+        } 
+
+
+        
+    }
+
+    static inputError() {
+        const div = document.createElement('div');
+        div.className = this.errorClass
+        div.appendChild(document.createTextNode('Please enter a cocktail name.'));
+        const container = document.querySelector('#input-container');
+        console.log(container);
+        const input = document.querySelector('#input-form');
+        console.log(input);
+        container.insertBefore(div, input);
+        setTimeout(() => document.querySelector('.alert').remove(), 3000);
+    }
+
+    static noCocktailError() {
+        const div = document.createElement('div');
+        div.className = this.errorClass;
+        div.appendChild(document.createTextNode('Sorry, no cocktail with that name was found.'));
+        const container = document.querySelector('#input-container'); 
+        const input = document.querySelector('#input-form');
+        container.insertBefore(div, input);
+        setTimeout(() => document.querySelector('.alert').remove(), 3000);
+    }
+}
+
 async function getCocktail(e) {
     e.preventDefault();
     console.log(searchBtn);
     let value = document.getElementById('search-input').value;
     console.log(value);
-    let url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${value}`;
-    console.log(url);
-    let response = await fetch(url);
-    let data = await response.json();
-    console.log(data);
-    UI.displaySearchedDrinks(data);
+    if (value === '') {
+        Error.inputError();
+    }
+    else {
+        try {
+            let url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${value}`;
+            console.log(url);
+            let response = await fetch(url);
+            let data = await response.json();
+            if (data.drinks === null) {
+                Error.noCocktailError();
+                return;
+            }
+            console.log(data);
+            UI.displaySearchedDrinks(data);
+        }
+        catch (error) {
+            console.log(error);
+            Error.displaySearchError();
+        }
+    }
 }
 
 async function getAllDrinks() {
-    let url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=`;
-    let response = await fetch(url);
-    let data = await response.json();
-    console.log(data);
-    UI.displayAllDrinks(data);
+    try {
+        let url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=`;
+        let response = await fetch(url);
+        let data = await response.json();
+        console.log(data);
+        UI.displayAllDrinks(data);
+    }
+    catch (error) {
+        console.log(error);
+        Error.displaySearchError();
+    }
 }
 
 async function getRandomDrink() {
-    let url = `https://www.thecocktaildb.com/api/json/v1/1/random.php`;
-    let response = await fetch(url);
-    let data = await response.json();
-    console.log(data);
-    UI.displayRandomDrink(data);
+    try {
+        let url = `https://www.thecocktaildb.com/api/json/v1/1/random.php`;
+        let response = await fetch(url);
+        let data = await response.json();
+        console.log(data);
+        UI.displayRandomDrink(data);
+    }
+    catch (error) {
+        console.log(error);
+        Error.displaySearchError();
+    }
 }
 
 // TODO: clicking see more brings out modal with more info
-
